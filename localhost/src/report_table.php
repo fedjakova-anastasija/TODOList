f14a333e1bcd7a04014399b3d05cc4f3fc25211a
Федякова Анастасия <fedyakova.anastasia@gmail.com>
2018-12-28 03:13:40 +0300

    Добавила кнопку

--- localhost/src/report_table.php
+++ localhost/src/report_table.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-require_once("common/view.php");
-report();
+/*require_once("common/view.php");
+report();*/
 require_once("../include/database.inc.php");
 require_once("common/check_sign_in_errors.php");
 require_once("common/view.php");
